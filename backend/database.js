a0958ac053cab825e005664d47a5505d0883f696
Amey Sanyal <amzicom123@gmail.com>
2024-12-14 10:59:00 +0530

    added start script

--- backend/database.js
+++ backend/database.js
@@ -2,8 +2,6 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-
-
 // Configure Sequelize with correct credentials
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
